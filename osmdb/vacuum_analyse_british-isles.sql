62099b16213432980bbb7bd3b6bd391817e69261
Oliver O'Brien <oobrien@users.noreply.github.com>
2019-03-20 15:46:01 +0000

    Add vacuum analyze files.
    These are called straight after rebuilding the db.

---
+++ osmdb/vacuum_analyse_british-isles.sql
@@ -0,0 +1,4 @@
+vacuum analyze uk_osm_polygon;
+vacuum analyze uk_osm_roads;
+vacuum analyze uk_osm_line;
+vacuum analyze uk_osm_point;
---
+++ osmdb/vacuum_analyse_denmark.sql
@@ -0,0 +1,4 @@
+vacuum analyze dk_osm_polygon;
+vacuum analyze dk_osm_roads;
+vacuum analyze dk_osm_line;
+vacuum analyze dk_osm_point;
