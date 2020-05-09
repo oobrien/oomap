d1db3e165f48c3ef1c57370f345d78bec5396191
Oliver O'Brien <o.obrien@ucl.ac.uk>
2020-05-09 10:54:33 +0100

    New URI for UK postboxes
    Updated link to Dracost UK postboxes API. Fixes #49

--- www/oomap.co.uk/gb/getpostboxes.php
+++ www/oomap.co.uk/gb/getpostboxes.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 $bounds = $_GET['bounds'];
-$file = file_get_contents("http://dracos.co.uk/made/nearest-postbox/nearest.php?bounds=$bounds");
+$file = file_get_contents("https://postboxes.dracos.co.uk/nearest.php?bounds=$bounds");
 echo $file;
 
 ?>
