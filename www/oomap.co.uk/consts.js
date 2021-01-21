4257a769da11a3510829680265abb44684d31a25
cadnant <david@ddixon.force9.co.uk>
2021-01-21 20:24:35 +0000

    Split out main.js constants.  Change post box retrieval code to use OSM boxes instead of Dracos boxes.

---
+++ www/oomap.co.uk/consts.js
@@ -0,0 +1,8 @@
+//Constants
+var prefix1 = "https://tiler4.oobrien.com/";
+var prefix2 = "https://tiler5.oobrien.com/";
+var prefix3 = "https://tiler6.oobrien.com/";
+var defaultMapTitle = "OpenOrienteeringMap";
+var defaultRaceDescription = "Race instructions";
+
+
